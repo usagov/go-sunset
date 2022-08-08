9a5b575b032b47a5bb0b6031daafde979aa63010
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2022-08-08 16:30:52 -0400

    Updated from USWDS2 to USWDS3

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,7 +1,9 @@
-const uswds = require('@uswds/compile');
-uswds.paths.dist.theme = './_theme';
+const uswds = require("@uswds/compile");
+uswds.settings.version = 3;
+uswds.paths.dist.theme = "./_theme";
+
 exports.init = uswds.init;
 exports.compile = uswds.compile;
 exports.watch = uswds.watch;
 
-uswds.paths.src.projectSass = './assets/css';
+uswds.paths.src.projectSass = "./assets/css";
